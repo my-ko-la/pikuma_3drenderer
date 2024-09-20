eeee661c7b85bcd9e2343e7037b2044397d571cd
Mykola Brechko <mykola.brechko@flip.shop>
2024-09-20 18:17:44 +0200

    render yellow window

--- 3drenderer/src/main.c
+++ 3drenderer/src/main.c
@@ -7,9 +7,31 @@ SDL_Renderer *renderer = NULL;
 bool is_running = false;
 
 void setup(void) {}
-void process_input(void) {}
+
+void process_input(void) {
+  SDL_Event event;
+  SDL_PollEvent(&event);
+
+  switch (event.type) {
+  case SDL_QUIT:
+    is_running = false;
+    break;
+  case SDL_KEYDOWN:
+    if (event.key.keysym.sym == SDLK_ESCAPE) {
+      is_running = false;
+    }
+    break;
+  }
+}
+
 void update(void) {}
-void render(void) {}
+
+void render(void) {
+  SDL_SetRenderDrawColor(renderer, 214, 250, 78, 255);
+  SDL_RenderClear(renderer);
+
+  SDL_RenderPresent(renderer);
+}
 
 bool initialize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -27,7 +49,6 @@ bool initialize_window(void) {
     return false;
   }
 
-  // TODO: Create SDL Renderer
   renderer = SDL_CreateRenderer(window, -1, 0);
 
   if (!renderer) {
@@ -44,7 +65,7 @@ int main(void) {
 
   setup();
 
-  while (true) {
+  while (is_running) {
     process_input();
     update();
     render();
