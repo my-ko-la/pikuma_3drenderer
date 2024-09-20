7e18a1ea5c9b2b5c257db5731521a0146b5591e6
Mykola Brechko <mykola.brechko@flip.shop>
2024-09-20 22:16:11 +0200

    preparing to render

--- 3drenderer/src/main.c
+++ 3drenderer/src/main.c
@@ -1,12 +1,29 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 bool is_running = false;
+uint32_t *color_buffer = NULL;
+SDL_Texture *color_buffer_texture = NULL;
 
-void setup(void) {}
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+const int RED = 0xFFFF0000;
+
+void setup(void) {
+  color_buffer =
+      (uint32_t *)malloc(sizeof(color_buffer) * WINDOW_WIDTH * WINDOW_HEIGHT);
+
+  if (color_buffer) {
+    color_buffer[(WINDOW_WIDTH * 10) + 20] = RED;
+  } else {
+    fprintf(stderr, "Buy more RAM.\n");
+  }
+}
 
 void process_input(void) {
   SDL_Event event;
@@ -26,11 +43,35 @@ void process_input(void) {
 
 void update(void) {}
 
+void clear_color_buffer(uint32_t color) {
+  for (int y = 0; y < WINDOW_HEIGHT; y++) {
+    for (int x = 0; x < WINDOW_WIDTH; x++) {
+      color_buffer[(WINDOW_WIDTH * y) + x] = color;
+    }
+  }
+}
+
 void render(void) {
   SDL_SetRenderDrawColor(renderer, 214, 250, 78, 255);
   SDL_RenderClear(renderer);
 
+  clear_color_buffer(0xFFFFFF00);
+
+  // Create a SDL texutre that is used to display the color buffer
+
+  color_buffer_texture = SDL_CreateTexture(
+      renderer, SDL_PIXELFORMAT_ARGB8888,
+      SDL_TEXTUREACCESS_STREAMING, // Frame by frame dynamic updates
+      WINDOW_WIDTH, WINDOW_HEIGHT);
+
   SDL_RenderPresent(renderer);
+}
+
+void render_color_buffer() {
+  SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer,
+                    (int)(WINDOW_WIDTH * sizeof(uint32_t)));
+
+  SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
 }
 
 bool initialize_window(void) {
@@ -39,10 +80,9 @@ bool initialize_window(void) {
     return false;
   }
 
-  /* Create SDL window */
   window =
       SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       800, 600, SDL_WINDOW_BORDERLESS);
+                       WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_BORDERLESS);
 
   if (!window) {
     fprintf(stderr, "ERROR: Failed to create SDL window.\n");
@@ -59,6 +99,14 @@ bool initialize_window(void) {
   return true;
 }
 
+void destroy_window() {
+  free(color_buffer);
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
+
 int main(void) {
 
   is_running = initialize_window();
@@ -70,6 +118,8 @@ int main(void) {
     update();
     render();
   }
+
+  destroy_window();
 
   return 0;
 }
